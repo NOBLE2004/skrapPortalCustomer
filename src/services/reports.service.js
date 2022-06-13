247a84a7742722ef63b76451ed2d984d3db556c4
yasirabbas <support@celvas.com.pk>
2022-06-13 13:46:12 +0500

    updates

--- src/services/reports.service.js
+++ src/services/reports.service.js
@@ -3,6 +3,7 @@ import HttpService from "./http.service";
 class ReportsService extends HttpService {
     getHireBreakdown = (data) => this.get("customers/report/hireBreakDown", data);
     getSitesBreakdown = (data) => this.get("customers/report/sitesBreakDown", data);
+    getJobsMeta = (data) => this.get("customers/report/jobsMeta", data);
 }
 
 export default new ReportsService();
