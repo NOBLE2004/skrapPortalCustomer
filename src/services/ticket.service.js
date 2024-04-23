c0c6c9f36587a39f52287e426976ce1ffc5c658e
zainawan <zainawan78@gmail.com>
2024-04-23 19:18:54 +0100

    updates

--- src/services/ticket.service.js
+++ src/services/ticket.service.js
@@ -3,7 +3,7 @@ import HttpService from "./http.service";
 class TicketService extends HttpService {
 
     list = (params) => this.get('tickets', params);
-    download = (params) => this.get('tickets/download', params);
+    download = (params) => this.getportal('tickets/download', params);
 }
 
 export default new TicketService();
