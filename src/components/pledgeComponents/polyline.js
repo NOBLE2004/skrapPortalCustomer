44cb53ca2dcdffad3f37ed1da25fc7b26005cc28
zainawan <zainawan78@gmail.com>
2024-05-13 11:51:31 +0100

    updates

--- src/components/pledgeComponents/polyline.js
+++ src/components/pledgeComponents/polyline.js
@@ -1,14 +1,21 @@
 import React from "react";
 import { Card, CardContent } from "@mui/material";
 import MainMap from "../map/MainMap";
-import { Polyline } from "react-google-maps";
+import { GoogleMap, Polyline } from "react-google-maps";
 import "../jobsDetail/findPostCode/findpostcode.scss";
+import mapStyles from "../map/mapStyles";
 const PolylineComp = ({start_lat, start_lng, destination_lat, destination_lng}) => {
     return (
         <div className="postcode-main">
             <Card className="driver-mapCard">
                 <CardContent>
-                    <MainMap
+                    <GoogleMap
+                        defaultZoom={14}
+                        defaultCenter={{
+                            lat: start_lat,
+                            lng: start_lng,
+                        }}
+                        defaultOptions={{ styles: mapStyles }}
                         googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyA6AYxz5ok7Wkt3SOsquumACIECcH933ws`}
                         loadingElement={<div style={{ height: `100%` }} />}
                         containerElement={<div style={{ height: `100%` }} />}
@@ -24,7 +31,7 @@ const PolylineComp = ({start_lat, start_lng, destination_lat, destination_lng}) 
                             strokeColor="#0000FF"
                             strokeOpacity={0.8}
                             strokeWeight={2} />
-                    </MainMap>
+                    </GoogleMap>
                 </CardContent>
             </Card>
         </div>
